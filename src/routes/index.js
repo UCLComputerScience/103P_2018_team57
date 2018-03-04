7c386e1479a7d96b7b6df4ed214986125135dedf
hirurana <zcabhra@ucl.ac.uk>
2018-03-04 01:15:20 +0000

    adding new version fitted to clients new requirements

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,33 @@
 module.exports = function(app) {
-	const videos = {
-		"Video 1": ["xxx", "nsfw", "saucy"],
-		"Video 2": ["animals"]
+	const projects = {
+		"project-1": {
+			projectName: "Project 1",
+			projectManager: "Bob Smith",
+			teamMembers: ["John Doe ", " Sangay "]
+		},
+		"project-2": {
+			projectName: "Project 2",
+			projectManager: "Johnny Appleseed",
+			teamMembers: [" Jo ", " Hey "]
+		}
 	}
 
 	app.get("/", function(req, res) {
-		res.render("index.html", {
-			videos: videos
+		res.redirect("/project/project-1")
+	})
+
+	app.get("/project/:projectID", function(req, res) {
+		res.render("project.html", {
+			projects: projects,
+			project: projects[req.params.projectID],
+			currentProjectID: req.params.projectID
 		})
 	})
 
-	app.post("/add-video", function(req, res) {
-		const videoName = req.body.videoName
-		const tags = req.body.tags.split(" ")
-		videos[videoName] = tags
-		res.redirect("/")
-	})
+	// app.post("/add-video", function(req, res) {
+	// 	const videoName = req.body.videoName
+	// 	const tags = req.body.tags.split(" ")
+	// 	videos[videoName] = tags
+	// 	res.redirect("/")
+	// })
 }
